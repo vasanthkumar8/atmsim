1d1a85199c1308d79ed2e5f7fe5591b84a5296f8
vasanthkumar8 <vasanth.831995@gmail.com>
2017-05-02 11:29:55 +0530

    Add files via upload

---
+++ AtmSimulator.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package atm;
+
+/**
+ *
+ * @author windows 8
+ */
+/**
+ * AWT Sample application
+ *
+ * @author 
+ * @version 1.00 07/02/02
+ */
+public class AtmSimulator {
+    
+    public static void main(String[] args) {
+        // Create application frame.
+        AtmSimulatorFrame frame = new AtmSimulatorFrame();
+        
+        // Show frame
+        frame.setVisible(true);
+    }
+}
